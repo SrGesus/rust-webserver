9d1163bc219202e087395c0026364421e861f4ba
SrGesus <12833047-SrGesus@users.noreply.gitlab.com>
2022-11-15 15:21:21 +0000

    POST requests functional for curl

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ These are both pretty fast tools and so should the webserver, provided my code d
 * [ ] Receive http requests with the following variables Latitude, Longitude, Speed, Time
     * [x] Handle put_data GET requests (string queries)
     * [ ] Handle put_data POST requests (multipart data)
+        * [x] Handle the curl's POST requests
+        * [ ] Handle the script's POST requests
 * [x] Provide an api that returns all the data already received - get_data GET requests
 * [ ] API description
 * [ ] **Bonus**: add persistence
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use warp::multipart::FormData;
-use warp::{self, Filter};
+use warp::multipart::{FormData, Part};
+use futures::TryStreamExt;
+
+use warp::{self, Filter, Buf};
 use std::collections::HashMap;
 use std::convert::Infallible;
+use std::io::Read;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use serde_json;
@@ -34,7 +37,27 @@ fn get_routes(
     db: Db
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     get_data(db.clone())
-        .or(put_data(db.clone()))
+        .or(put_data(db))
+}
+
+// /get_data
+fn get_data(
+    db: Db
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path("get_data")
+        .and(with_db(db))
+        .and_then(handler_get_data)
+}
+
+async fn handler_get_data(
+    db: Db
+) -> Result<impl warp::Reply, Infallible> {
+    let db = db.lock().await;
+    
+    let json_table = serde_json::to_string_pretty(&db.clone()).unwrap();
+
+    println!("get_data GET request: {}\n", json_table);
+    Ok(json_table)
 }
 
 // /put_data
@@ -47,40 +70,74 @@ fn put_data(
         .and(warp::get()
             .and(warp::query::<HashMap<String, String>>())
             .and(with_db(db.clone()))
-            .and_then(handler_put_data_get))
+            .and_then(handler_put_data_get)
+        )
     
     // /put_data POST queries
         .or(warp::post()
             .and(warp::multipart::form())
             .and(with_db(db))
-            .and_then(handler_put_data_post))
-}
-
-// TODO
-// /get_data
-fn get_data(
-    db: Db
-)
--> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    warp::path("get_data")
-        .and(with_db(db))
-        .and_then(handler_get_data)
-}
-async fn handler_get_data(
-    db: Db
-) -> Result<impl warp::Reply, Infallible> {
-    let db = db.lock().await;
-    
-    let json_table = serde_json::to_string_pretty(&db.clone()).unwrap();
-
-    println!("get_data GET request: {}\n", json_table);
-    Ok(json_table)
+            .and_then(handler_put_data_post)
+        )
 }
 
 async fn handler_put_data_get(
     values: HashMap<String, String>,
     db: Db
 ) -> Result<impl warp::Reply, Infallible> {
+    
+    println!("put_data GET request: {:?}\n", values);
+
+    let result = handler_put_data(values, db).await;
+
+    Ok(result)
+}
+
+// TODO (don't know how)
+async fn handler_put_data_post(
+    values: FormData,
+    db: Db
+) -> Result<impl warp::Reply, warp::Rejection> {
+
+    // Collect all the parts of the multiform data and wait for the futures
+    let mut parts: Vec<Part> = values.try_collect().await.map_err(|err| {
+        eprintln!("form error: {}", err);
+        warp::reject::reject()
+    })?;
+
+    println!("put_data POST request: {:?}", parts);
+
+    // Extract the data from the FormData and turn it into a HashMap
+    let mut hash_map: HashMap<String, String> = HashMap::new();
+
+    for part in parts.iter_mut() {
+        let name = String::from(part.name());
+        if let Ok(buf) = part.data().await.unwrap() {
+            let mut reader = buf.reader();
+            let mut dst = [0; 1024];
+
+            let len = reader.read(&mut dst).unwrap();
+            let data = String::from_utf8_lossy(&dst[0..len]).into_owned();
+
+            println!("{} = {}", name, data);
+            hash_map.insert(name, data);
+        } else {
+            println!("{} data collection failed", part.name());
+        }
+        
+    }
+
+    // Push into the Db and get the added data
+    let result = handler_put_data(hash_map, db).await;
+
+    Ok(result)
+}
+
+async fn handler_put_data (
+    values: HashMap<String, String>,
+    db: Db
+) -> String {
+
     let mut result = String::from("");
     let mut db = db.lock().await;
 
@@ -93,24 +150,13 @@ async fn handler_put_data_get(
         }
     };
 
-    println!("put_data GET request: {:?}\n", values);
-    Ok(result)
+    result
 }
 
-// TODO (don't know how)
-async fn handler_put_data_post(
-    values: FormData,
-    db: Db
-) -> Result<impl warp::Reply, Infallible> {
-    
-    //println!("put_data POST request: {}");
-    Ok(format!("{:?}", values))
-}
-
-
-// adds the database as an argument when used with .and method in routes
+// Adds the database as an argument when used with .and method in routes
 fn with_db(
     db: Db
 ) -> impl Filter<Extract = (Db,), Error = Infallible> + Clone {
     warp::any().map(move || db.clone())
 }
+
